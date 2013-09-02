f5b4fa6481131d1807564d37878077b99143e0a3
lordgeorg <lordgeorg@googlemail.com>
2013-09-02 17:00:18 +0200

    introduces Statement::parse() and statement_ptr_t

--- src/bin/sql-parser/Statement.h
+++ src/bin/sql-parser/Statement.h
@@ -1,10 +1,18 @@
 // Copyright (c) 2012 Hasso-Plattner-Institut fuer Softwaresystemtechnik GmbH. All rights reserved.
-#ifndef SRC_BIN_SQL_PARSER_STATEMENT_H_
-#define SRC_BIN_SQL_PARSER_STATEMENT_H_
+#ifndef SRC_BIN_SQL_PARSER_STATEMENT_H
+#define SRC_BIN_SQL_PARSER_STATEMENT_H
+
+#include <string>
+#include <memory>
+
+class Statement;
+typedef std::shared_ptr<Statement> statement_ptr_t;
 
 class Statement {
 
+public:
+  static statement_ptr_t parse(std::string query);
 };
 
-#endif //  SRC_BIN_SQL_PARSER_STATEMENT_H_
+#endif // SRC_BIN_SQL_PARSER_STATEMENT_H
 
